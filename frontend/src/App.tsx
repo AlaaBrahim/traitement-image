a535e2cf3f07e2c3d015c10ab1f222a31ff11c91
Alaa Brahim <alaa.brahim@etudiant-fst.utm.tn>
2024-05-01 21:04:11 +0100

    add shadcn to frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Button } from "@/components/ui/button"
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +11,8 @@ function App() {
   return (
     <>
       <div>
+        <Button>Click me</Button>
+
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
